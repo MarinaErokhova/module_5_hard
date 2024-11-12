5950db7706d882f7f871306c77785d9e6e94bd0f
MarinaErokhova <erokhovamn@gmail.com>
2024-11-12 22:44:51 +0300

    Update module5hard.py

--- module5hard.py
+++ module5hard.py
@@ -1,6 +1,6 @@
 import time
 
-class user:
+class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
         self.password = hash(password)
@@ -8,7 +8,7 @@ class user:
     def __str__(self):
         return self.nickname
 
-class video:
+class Video:
     def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
@@ -16,7 +16,7 @@ class video:
         self.adult_mode = adult_mode
 
     def __eq__(self, other):
-        if isinstance(other, video):
+        if isinstance(other, Video):
             if self.title == other.title and self.duration == other.duration:
                 return True
         return False
@@ -34,12 +34,20 @@ class UrTube:
                 break
 
     def register(self, nickname, password, age):
-        for user in self.users:
-            if user.nickname == nickname:
-                print(f'Пользователь {nickname} уже существует')
-                break
+        print("gg1")
+        if not self.users:
+            self.users.append(User(nickname, hash(password), age))
+            self.current_user = User(nickname, hash(password), age)
+
+            for user in self.users:
+                print("gg2")
+                if user.nickname == nickname:
+                    print(f'Пользователь {nickname} уже существует')
+                    break
             else:
-                self.users.append((nickname, hash(password), age))
+                print("gg3")
+                temp_user = User(nickname, hash(password), age)
+                self.users.append(temp_user)
                 self.current_user = nickname
 
     def log_out(self):
@@ -60,12 +68,11 @@ class UrTube:
         return list_video
 
     def watch_video(self, film_name):
-        if self.current_user is None:
-            print("Войдите в аккаунт, чтобы смотреть видео")
-            return
-
         for i in self.videos:
-            if film_name == i.title:
+            if self.current_user is None:
+                print("Войдите в аккаунт, чтобы смотреть видео")
+                return
+            elif film_name == i.title:
                 if self.current_user.age < 18:
                     print("Вам нет 18 лет, пожалуйста покиньте страницу")
                     return
@@ -79,8 +86,8 @@ class UrTube:
 
 
 ur = UrTube()
-v1 = video('Лучший язык программирования 2024 года', 200)
-v2 = video('Для чего девушкам парень программист?', 10, adult_mode=True)
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
 # Добавление видео
 ur.add(v1, v2)
